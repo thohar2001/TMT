acf587bd9a72ac0ff532cc68796b06e062cf89bb
thohar2001 <72736557+thohar2001@users.noreply.github.com>
2021-01-29 14:37:51 +0100

    Added new Maven project
    Initialized maven project

---
+++ maven-addressbook/src/main/java/se/yrgo/maven_addressbook/App.java
@@ -0,0 +1,13 @@
+package se.yrgo.maven_addressbook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
