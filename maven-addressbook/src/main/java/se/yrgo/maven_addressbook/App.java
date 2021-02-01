8fe580a42267112b71222c3f44b22efcaea0792a
Tommi Ahonen <tomaho1129@skola.goteborg.se>
2021-02-01 15:33:36 +0100

    Create Main-method.

--- maven-addressbook/src/main/java/se/yrgo/maven_addressbook/App.java
+++ maven-addressbook/src/main/java/se/yrgo/maven_addressbook/App.java
@@ -6,8 +6,17 @@ package se.yrgo.maven_addressbook;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        AddressBook aBook = new AddressBook();
+        Contact c1 = new Contact("George", "Costanza", 50, "New York city", 55516424, "myemail@hotmail.com");
+        Contact c2 = new Contact("Jerry", "Seinfeld", 45, "New York city", 55516344, "jerrymillions@hotmail.com");
+        aBook.addContact(c1);
+        aBook.addContact(c2);
+        try {
+        	Contact c = aBook.findContact("jerrymillions@hotmail.com");
+        	System.out.println("Found contact: " + c.getFirstName() + " " + c.getLastName());
+        } catch (Exception e) {
+			System.out.print(e.getMessage());
+		}
     }
 }
