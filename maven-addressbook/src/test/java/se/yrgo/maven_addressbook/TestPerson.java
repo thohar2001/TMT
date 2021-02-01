40d68b023d51c82d71657521fac3df30f922039f
Mehrdadbadeie <72803122+Mehrdadbadeie@users.noreply.github.com>
2021-02-01 09:46:40 +0100

    Create TestClass
    Mehrdad created a Junit TestClass for Personclass

---
+++ maven-addressbook/src/test/java/se/yrgo/maven_addressbook/TestPerson.java
@@ -0,0 +1,14 @@
+package se.yrgo.maven_addressbook;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPerson {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
