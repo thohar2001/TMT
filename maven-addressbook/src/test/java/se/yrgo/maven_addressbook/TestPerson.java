79e8a22cc9db540dae162760f5afc4f161d45b80
Mehrdadbadeie <72803122+Mehrdadbadeie@users.noreply.github.com>
2021-02-01 11:00:48 +0100

    Update TestPerson.java
    changed the name of testGetPersonIgnoreCase.
    
    added another test class

--- maven-addressbook/src/test/java/se/yrgo/maven_addressbook/TestPerson.java
+++ maven-addressbook/src/test/java/se/yrgo/maven_addressbook/TestPerson.java
@@ -7,9 +7,15 @@ import org.junit.Test;
 public class TestPerson {
 
 	@Test
-	public void testGetPersonIgnoringCase() {
+	public void testgetFirstNameIgnoringCase() {
 		Person person1 = new Person("BoSSe", "Bossesson", 12);
 		assertTrue(person1.getFirstName().equalsIgnoreCase("bosse"));
+	}
+	
+	@Test
+	public void testgetFirstName() {
+		Person person1 = new Person("Bosse", "Bossesson", 12);
+		assertEquals(person1.getFirstName(), "Bosse");
 	}
 	
 }
