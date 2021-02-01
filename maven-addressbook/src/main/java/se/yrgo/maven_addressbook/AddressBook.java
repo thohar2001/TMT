f5a12dcb414cbb2ce4ddaaab8eb043152a7d63ef
thohar2001 <72736557+thohar2001@users.noreply.github.com>
2021-02-01 11:30:12 +0100

    Revert "Added more methods and tests"

--- maven-addressbook/src/main/java/se/yrgo/maven_addressbook/AddressBook.java
+++ maven-addressbook/src/main/java/se/yrgo/maven_addressbook/AddressBook.java
@@ -14,25 +14,12 @@ public class AddressBook {
 		this.contacts = new ArrayList<>(contacts);
 	}
 	
-	public int getNumberOfContacts() {
-		return contacts.size();
-	}
-	
 	public void addContact(Contact contact) {
 		this.contacts.add(contact);
 	}
 	
     public void removeContact(Contact contact) {
     	this.contacts.remove(contacts.indexOf(contact));
-    }
-    
-    public Contact findContact(String email) throws Exception {
-    	for(int i=0; i<contacts.size(); i++) {
-    		if (contacts.get(i).getEmail().equals(email)) {
-    			return contacts.get(i);
-    		}
-    	}
-    	throw new Exception("Contact with email: " + email + " not found");
     }
     
 }
--- maven-addressbook/src/test/java/se/yrgo/maven_addressbook/TestAddressBook.java
+++ maven-addressbook/src/test/java/se/yrgo/maven_addressbook/TestAddressBook.java
@@ -5,36 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestAddressBook {
-	private AddressBook ab;
-	private Contact testContact;
-	
-	public TestAddressBook() {
-		ab = new AddressBook();
-		testContact = new Contact("Jonas", "Karlsson", 18, "Gamla Gatan 4", 12345, "jonas.karlsson@gmail.com");
-		ab.addContact(testContact);
-	}
 
 	@Test
-	public void testGetNumberOfContacts() {
-		assertEquals(ab.getNumberOfContacts(), 1);
-	}
-	
-	@Test
-	public void testAddContact() {
-		Contact contact = new Contact("Karl", "Jonasson", 35, "Nya Vägen 3", 23456, "karl.jonasson@gmail.com");
-		ab.addContact(contact);
-		assertEquals(ab.getNumberOfContacts(), 2);
-	}
-	
-	@Test
-	public void testRemoveContact() {
-		ab.removeContact(testContact);
-		assertEquals(ab.getNumberOfContacts(), 0);
-	}
-	
-	@Test
-	public void testFindContact() throws Exception {
-		assertTrue(ab.findContact("jonas.karlsson@gmail.com").equals(testContact));
+	public void test() {
+		fail("Not yet implemented");
 	}
 
 }
